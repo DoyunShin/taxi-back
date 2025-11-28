ac65354d7a578874c5534dd38988f1a617137fb8
thxx132 <thxx@sparcs.org>
2025-11-28 16:14:34 +0900

    Add: getMiniGameInfoHandler

--- src/miniGame/routes/miniGames.ts
+++ src/miniGame/routes/miniGames.ts
@@ -1,5 +1,11 @@
+import { authMiddleware } from "@/middlewares";
+import * as miniGameHandlers from "../services/miniGames";
 import express from "express";
 
 const router = express.Router();
+
+router.use(authMiddleware);
+
+router.get("/", miniGameHandlers.getMiniGameInfosHandler);
 
 export default router;
--- src/miniGame/services/miniGames.ts
+++ src/miniGame/services/miniGames.ts
@@ -1,4 +1,31 @@
 import type { RequestHandler } from "express";
 import { miniGameModel } from "../modules/mongo";
+import { getLoginInfo, isLogin } from "@/modules/auths/login";
+import logger from "@/modules/logger";
 
-const getUserData: RequestHandler = async (req, res) => {};
+export const getMiniGameInfosHandler: RequestHandler = async (req, res) => {
+  try {
+    const userId = isLogin(req) ? getLoginInfo(req).oid : null;
+    const miniGameStatus = await miniGameModel.findOne({ userId }).lean();
+    if (!miniGameStatus) {
+      const newMiniGameStatus = new miniGameModel({
+        userId: req.userOid,
+        powerUnitLevel: 1,
+        frameLevel: 1,
+        tyreLevel: 1,
+        creditAmount: 0,
+        updatedAt: new Date(),
+      });
+      await newMiniGameStatus.save();
+      return res.json({
+        newMiniGameStatus,
+      });
+    }
+    return res.json({
+      miniGameStatus,
+    });
+  } catch (err) {
+    logger.error(err);
+    res.status(500).json({ error: "GlobalState/ : internal server error" });
+  }
+};
